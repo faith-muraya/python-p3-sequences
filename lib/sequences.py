79725a9e73bf3e376e62f9f93cd969bbf774d7bb
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-11-24 12:45:36 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_seq = []
+    if length >= 1:
+        fib_seq.append(0)
+    if length >= 2:
+        fib_seq.append(1)
+    if length >= 3:
+        a, b = 0, 1
+        for _ in range(length-2):
+            fib_seq.append(a + b)
+            a, b = b, a + b
+    print(fib_seq)
